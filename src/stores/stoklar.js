90ed7a902f3892edec7e616e5c193908335b5434
Hakan Bilgi <hknblg34@gmail.com>
2023-07-03 21:41:04 +0300

    change

--- src/stores/stoklar.js
+++ src/stores/stoklar.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { v4 as uuid } from "uuid";
 
 export const useStokStore = defineStore("stoklar", {
-    
+
     state: () => {
         return {
             stoklar: []
